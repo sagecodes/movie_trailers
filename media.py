01327c9183be22d0ae58d6149d41e8ed8a435bca
Sage Elliott <wildtroll@gmail.com>
2015-07-14 15:05:18 -0700

    add cast attribute to movie class. Update all moves in entertainment.py to include cast. add comment listing order of movie attributes to easily fill out.

--- media.py
+++ media.py
@@ -2,8 +2,9 @@
 class Movie():
 	""" This class provides a way to store movie related information """
 	# Create Movie Attributes
-	def __init__(self, movie_title, movie_storyline, poster_image_url, trailer_youtube_url):
+	def __init__(self, movie_title, movie_storyline, movie_cast, poster_image_url, trailer_youtube_url):
 		self.title = movie_title
 		self.storyline = movie_storyline
+		self.cast = movie_cast
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url

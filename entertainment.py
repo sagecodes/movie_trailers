1fadf8283447503afd59054d1e8975c792284437
Sage Elliott <wildtroll@gmail.com>
2015-07-14 17:28:08 -0700

    update them apples to include storyline and cast info

--- entertainment.py
+++ entertainment.py
@@ -3,37 +3,37 @@ import them_apples
 
 # Movie Attributes order: title, storyline, cast, image URL, trailer URL.
 kung_fury = media.Movie(
-          "kung Fury",
+          "Kung Fury",
           "Kung fu guy becomes time traveling nazi killer",
-          "that kung fu guy",
+          "That kung fu guy",
           "https://upload.wikimedia.org/wikipedia/en/thumb/0/0d/Kung_Fury_Poster.png/220px-Kung_Fury_Poster.png",  # NOQA
           "https://www.youtube.com/watch?v=72RqpItxd8M")
 
 batman_begins = media.Movie(
               "Batman Begins",
               "Rich guy becomes coolest ninja ever",
-              "that risc guy",
+              "That rich guy",
               "https://upload.wikimedia.org/wikipedia/en/thumb/a/af/Batman_Begins_Poster.jpg/220px-Batman_Begins_Poster.jpg",  # NOQA
               "https://www.youtube.com/watch?v=vak9ZLfhGnQ")
 
 iron_man = media.Movie(
           "Iron Man",
           "Rich guy becomes coolest metal man ever",
-          "that rich guy, no the OTHER rich guy",
+          "That rich guy, no the OTHER rich guy",
           "https://upload.wikimedia.org/wikipedia/en/thumb/7/70/Ironmanposter.JPG/220px-Ironmanposter.JPG",  # NOQA
           "https://www.youtube.com/watch?v=8hYlB38asDY")
 
 starwars = media.Movie(
           "Starwars Return of the Jedi",
           "whiny guy becomes powerful jedi",
-          "that whiny guy",
+          "That whiny guy",
           "https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/ReturnOfTheJediPoster1983.jpg/220px-ReturnOfTheJediPoster1983.jpg",  # NOQA
           "https://www.youtube.com/watch?v=7L8p7_SLzvU")
 
 star_trek = media.Movie(
           "Star Trek",
           "Cocky guy becomes captain of a starship",
-          "that cocky guy",
+          "That cocky guy",
           "https://upload.wikimedia.org/wikipedia/en/thumb/2/29/Startrekposter.jpg/220px-Startrekposter.jpg",  # NOQA
           "https://www.youtube.com/watch?v=SyJszxnJydA")
 
--- them_apples.py
+++ them_apples.py
@@ -122,6 +122,9 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <h2>"{movie_storyline}"</h2>
+    <h3>Starring:</h3>
+    <h2>{movie_cast}</h2>
 </div>
 '''
 
@@ -137,6 +140,8 @@ def create_movie_tiles_content(movies):
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
+            movie_storyline=movie.storyline,
+            movie_cast=movie.cast,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
         )
